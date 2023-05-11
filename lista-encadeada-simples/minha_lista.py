8d7b203bd2dc755fce1b9e72fd036153ca88331e
tecnicotronfullstack@gmail.com <tecnicotronfullstack@gmail.com>
2023-05-10 21:11:29 -0300

    Melhorando o código.

--- lista-encadeada-simples/minha_lista.py
+++ lista-encadeada-simples/minha_lista.py
@@ -1,108 +1,142 @@
-class No:        
-    valor: object = None
-    proximo: 'No' = None
+class No:
+    def __init__(self, valor):        
+        self.valor: object = valor
+        self.proximo: 'No' = None
     
 class MinhaLista:
     #INICIALIZADOR 
     def __init__(self, *args):
-        self.no_raiz = No()
-        self.tamanho = 0     
-        self._adicionar_itens(*args)
-    #MÉTODOS AUXILIARES
-    def _buscar_no(self, indice):
-        no = self.no_raiz.proximo
-        for i in range(self.tamanho):
-            if i  == indice:
-                return no
-            no = no.proximo
-        return 
-    def _adicionar_itens(self,*args):
-        if len(args) != 0:
-            for i in range(len(args)):
-                item = args[i]
-                self.adicionar(item)    
-                                
-    #CRUD
-    def adicionar(self, valor):                        
-        no = self.no_raiz
-        ultimo_no = None        
-        for i in range(self.tamanho+1):
-            if no.proximo == ultimo_no:
-
-                novo_no = No()
-                novo_no.valor  = valor
-                
-                no.proximo = novo_no
-                self.tamanho += 1
-                
-                return self
-            else:
-                no = no.proximo                
-    def procurar(self, indice):
-        no = self._buscar_no(indice)
-        if no != None:
-            return no.valor
-        else:
-            print("Posição não está na lista")
-        return 
+        self.primeiro = None        
+        if args:    
+            self.preencher(args)
     
-    def atualizar(self, indice, valor): 
-        no = self._buscar_no(indice)
-        if no != None:
-            no.valor = valor
-            return self
+    #MÉTODOS AUXILIARES
+    def preencher(self, valores):
+        if not valores:
+            return
+                 
+        self.primeiro = No(valores[0])     
+        atual = self.primeiro
+        for i in range(1,len(valores)):
+            atual.proximo = No(valores[i])
+            atual = atual.proximo            	                    
+                                                                       
+    #CRUD
+    def adicionar(self, valor):                       
+        novo = No(valor)
+        if self.primeiro is None:
+            self.primeiro = novo
         else:
-            print("Posição não está na lista") 
-        return
+            atual = self.primeiro
+            while atual.proximo != None:
+                atual = atual.proximo        
+            atual.proximo = novo
+        
+        
+    def obter(self, indice):
+        atual = self.primeiro
+        i = 0
+        while atual is not None:    
+            if indice == i:
+                return atual.valor        
+            atual = atual.proximo 
+            i += 1
+        return None
+    
+    
+    def buscar(self, valor):                                
+        atual = self.primeiro
+        indice = 0
+        while atual is not None:         
+            if atual.valor == valor:
+                return indice
+            atual = atual.proximo 
+            indice += 1                
+        return -1
+    
+        
+    def atualizar(self, indice, valor):       
+        atual = self.primeiro
+        i = 0
+        while atual is not None:    
+            if indice == i:
+                atual.valor = valor
+            atual = atual.proximo 
+            i += 1 
+                  
     
     def remover(self, indice):
-        no = self.no_raiz
-        for i in range(self.tamanho):
-            if no.proximo != None: 
-                if i == indice:          
-                    proximo = no.proximo                    
-                    no.proximo = proximo.proximo
-                    self.tamanho -= 1                   
-                    return self                                                
-            no = no.proximo                                
-        return
+        if indice < 0:
+            return None        
+        
+        if indice == 0:
+            removido = self.primeiro
+            self.primeiro = removido.proximo
+            return removido.valor                    
+        
+        atual = self.primeiro        
+        i = 1
+        while atual is not None and i < indice :            
+            atual = atual.proximo 
+            i += 1             
+        
+        if atual is None or atual.proximo is None:
+            return None        
+        
+        removido = atual.proximo
+        atual.proximo = removido.proximo
+        return removido.valor
+    
+    def contem(self, valor):
+        atual = self.primeiro
+        while atual is not None:
+            if atual.valor == valor:
+                return True
+            atual = atual.proximo
+        return False
+        
+    
+    def contar(self):
+        atual = self.primeiro
+        contador = 0
+        while atual is not None:
+            contador += 1
+            atual = atual.proximo
+        return contador
+
     #MÉTODOS ESPECIAIS
-    def __str__(self):
-        string = "MinhaLista["                
-        no = self.no_raiz.proximo
-        if no == None:
-            string += "]"
-            return string
-        for i in range(self.tamanho):
-            valor = str(no.valor)
-            string += valor
-            if no.proximo != None:
-                string += ", "
-            else:
-                string += "]"
-                break
-            no = no.proximo
-        return string        
+    def __str__(self, nome_da_estrutura="MinhaLista"):
+        lista = []
+        atual = self.primeiro
+        while atual is not None:
+            lista.append(atual.valor)
+            atual = atual.proximo        
+                                
+        lista = str(lista)
+        return f"Lista{lista}"
         
     def __iter__(self):
-        no = self.no_raiz
-        for i in range(self.tamanho):
-            no = no.proximo           
-            yield no.valor
-    
+        no = self.primeiro
+        tamanho = self.contar()
+        i = 0
+        while no is not None:                               
+            yield no.valor 
+            no = no.proximo
+                                                                   
     def __len__(self):
-        return self.tamanho
+        return self.contar()
     
     def __getitem__(self, indice):
         if isinstance(indice, slice):
             inicio, fim, intervalo = indice.indices(len(self))
             lista = MinhaLista()
             for i in range(inicio, fim, intervalo):
-                lista.adicionar(self.procurar(i))
+                lista.adicionar(self.obter(i))
             return lista
         else:
-            return self.procurar(indice)
+            return self.obter(indice)
     
     def __setitem__(self, indice, valor):
         self.atualizar(indice, valor)
+
 
